5bd35f4c3fac52ee0940c0eb47fdeff56c890f41
jasonren5 <31326470+jasonren5@users.noreply.github.com>
2018-11-26 22:34:53 -0600

    got node and socketio running

---
+++ book-server.js
@@ -0,0 +1,21 @@
+var socketio = require('socket.io'),
+    url = require('url'),
+    sys = require('sys'),
+    express = require('express'),
+    http = require('http'),
+    path = require('path');
+
+
+var app = express();
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/client.html'));
+});
+app.use(express.static(path.join(__dirname, '/public')));
+var server = http.createServer(app);
+var socket = socketio.listen(server);
+
+server.listen(8080)
+var io = socketio.listen(server);
+io.sockets.on('connection', function (socket) {
+    console.log("user connected");
+});
