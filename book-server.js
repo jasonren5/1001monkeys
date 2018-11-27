b0c1435e71661841d90ddaa76054e348d6cff350
jasonren5 <31326470+jasonren5@users.noreply.github.com>
2018-11-26 23:59:44 -0600

    node successfully connects to mysql server

--- book-server.js
+++ book-server.js
@@ -1,14 +1,14 @@
 var socketio = require('socket.io'),
-    url = require('url'),
-    sys = require('sys'),
-    express = require('express'),
-    http = require('http'),
-    path = require('path');
+  url = require('url'),
+  sys = require('sys'),
+  express = require('express'),
+  http = require('http'),
+  path = require('path');
 
 
 var app = express();
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/client.html'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/client.html'));
 });
 app.use(express.static(path.join(__dirname, '/public')));
 var server = http.createServer(app);
@@ -17,19 +17,19 @@ var socket = socketio.listen(server);
 server.listen(8080)
 var io = socketio.listen(server);
 io.sockets.on('connection', function (socket) {
-    console.log("user connected");
+  console.log("user connected");
 });
 
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "http://ec2-18-191-98-21.us-east-2.compute.amazonaws.com",
-  port: "3306",
+  host: "localhost",
   user: "root",
-  password: "qwerty"
+  password: "qwerty",
+  database: "1001monkeys"
 });
 
-con.connect(function(err) {
+con.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 });
