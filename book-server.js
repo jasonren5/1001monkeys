c8ec2b3fb17ef15cc7c2a8f843c584f4cb0af99a
jasonren5 <31326470+jasonren5@users.noreply.github.com>
2018-11-27 18:24:47 -0600

    attempted registration

--- book-server.js
+++ book-server.js
@@ -14,22 +14,40 @@ app.use(express.static(path.join(__dirname, '/public')));
 var server = http.createServer(app);
 var socket = socketio.listen(server);
 
-server.listen(8080)
-var io = socketio.listen(server);
-io.sockets.on('connection', function (socket) {
-  console.log("user connected");
-});
-
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
+var sql = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "qwerty",
   database: "1001monkeys"
 });
 
-con.connect(function (err) {
+sql.connect(function (err) {
   if (err) throw err;
-  console.log("Connected!");
+  console.log("MySQL Connected!");
 });
+
+server.listen(8080)
+var io = socketio.listen(server);
+io.sockets.on('connection', function (socket) {
+  console.log("user connected");
+
+  socket.on('register-attempt', function (data) {
+    var username = data["username"];
+    var password = data["password"];
+    sql.query('SELECT USERNAME FROM users WHERE username = ?', username, function (err, result, fields) {
+      if (result.length() > 0) {
+        socket.emit('user-exists');
+      } else {
+        sql.query('INSERT INTO users (username, password, votes, bio) VALUES (' + username + ', ' + password + ', 0, "user has not set bio")', function (err, result) {
+          if (err) throw err;
+          console.log("1 user registered, successfully entered into table users");
+        });
+      }
+    });
+  });
+
+
+});
+
